dd43031ee483c1a5570475f534ef6440264ed582
Yizero <yizerowu@dnspod.com>
2013-03-15 11:44:20 +0800

    fix

--- lib/config.php
+++ lib/config.php
@@ -1,10 +1,10 @@
 <?php
 //应用ID编号
-define('APP_KEY', '');
+define('APP_KEY', '');
 
 //应用密钥
-define('APP_SECRET', '');
+define('APP_SECRET', '');
 
 //应用回调地址
-define('APP_CALLBACK', '');
+define('APP_CALLBACK', '');
 ?>
